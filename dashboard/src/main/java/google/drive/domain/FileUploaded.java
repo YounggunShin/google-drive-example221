0ce0982f6c021d43cbad05dd366bb0bf855d626b
YounggunShin <121846288+YounggunShin@users.noreply.github.com>
2023-02-21 13:09:58 +0900

    commit

---
+++ dashboard/src/main/java/google/drive/domain/FileUploaded.java
@@ -0,0 +1,15 @@
+package google.drive.domain;
+
+import google.drive.infra.AbstractEvent;
+import lombok.Data;
+import java.util.*;
+
+
+@Data
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private String path;
+    private Long size;
+}
